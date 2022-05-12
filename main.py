f0eb83a9cd1a95b0f1c7624901bea1b8330667fd
Alexandr Sbitnev <asbitnev@itsumma.ru>
2022-05-12 14:21:57 +0800

    add b64 decode ?

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import imaplib
+import base64
 
 email = 'ksenobit16bit@yandex.ru'
 password = 'harlaaqhkqaaaasy'
@@ -8,7 +9,7 @@ mail.login(email,password)
 mail.list()
 mail.select("&BB8EPgQ0BD8EOARBBEwEOgQ4-|&BC8EPQQ0BDUEOgRB-|taxi")
 
-result, data = mail.search(None, '(FROM "yandex.*")')
+result, data = mail.search(None, '(FROM "no-reply@taxi.yandex.ru")')
 
 ids = data[0]
 id_list = ids.split()
@@ -17,3 +18,6 @@ latest_email_id = id_list[-1]
 result, data = mail.fetch(latest_email_id, "(RFC822)")
 raw_email = data[0][1]
 raw_email_string = raw_email.decode('utf-8')
+print (raw_email_string)
+#decoded_mail = base64.b64decode(raw_email_string.encode("utf-8")).decode("utf-8")
+#print (decoded_mail)
